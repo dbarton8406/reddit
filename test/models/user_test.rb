c8d2c957cb071bdc57d43d61487fdb62aa65cb3b
Daniel <dbarton8406@gmail.com>
2015-11-10 23:54:15 -0500

    added test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+  test "should not save user without first_name" do
+    user = User.new
+    assert_not user.save
+  end
+
+   test "should not save user without last_name" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "should save user with first_name" do
+    user = User.new
+    refute_not user.save
+  end
+
+  test "should save user with last_name" do
+    user = User.new
+    refute_not user.save
+  end
 end
