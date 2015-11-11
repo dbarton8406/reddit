68067ddcc70dd316d6c5a4a4b6204833ecadca6e
Daniel <dbarton8406@gmail.com>
2015-11-11 11:10:03 -0500

    added passing test for user and commented ot fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,26 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
+
   test "should not save user without first_name" do
     user = User.new
-    assert_not user.save
+
+    refute user.save  
+   
   end
 
-   test "should not save user without last_name" do
+  test "should not save user without last_name" do
     user = User.new
-    assert_not user.save
+
+    refute user.save
   end
 
   test "should save user with first_name" do
     user = User.new
-    refute_not user.save
+
+    refute user.save
+    assert user.errors[:first_name].present?
   end
 
   test "should save user with last_name" do
     user = User.new
-    refute_not user.save
+
+    refute user.save
+    assert user.errors[:last_name].present?
+    
   end
 end
